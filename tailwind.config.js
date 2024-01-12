208b2cad1d4edee767ea41ba06c4eda19d50cb51
Mishieck <mishieckmwale@gmail.com>
2024-01-12 18:16:53 +0200

    chore(tailwind): add tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+const { fontFamily } = require('tailwindcss/defaultTheme');
+const colors = require('./src/styles/colors');
+
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px'
+      }
+    },
+    extend: { colors },
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+      serif: ['Roboto Serif', 'serif']
+    }
+  },
+  plugins: [
+    require('tailwindcss-react-aria-components')({ prefix: 'rac' }),
+    require('tailwindcss-animate')
+  ]
+};
