3431bd97329686f8d96d705290d74f1aadb5c094
Mishieck <mishieckmwale@gmail.com>
2024-01-12 18:09:19 +0200

    chore(storybook): configure storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,22 @@
+import type { Preview } from '@storybook/react';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import '@/index.css';
+import 'material-icons/iconfont/material-icons.css';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i
+      }
+    },
+    viewport: {
+      viewports: INITIAL_VIEWPORTS,
+      defaultViewport: 'iphone6'
+    }
+  }
+};
+
+export default preview;
