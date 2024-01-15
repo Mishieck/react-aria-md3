824e6f6b222d9ee0b686cdd920071ddfb7148708
Mishieck <mishieckmwale@gmail.com>
2024-01-15 13:12:17 +0200

    feat(components/icon): add ref

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -1,5 +1,6 @@
 import { MdIconName } from '@/types/material-icons';
 import { SizeShort } from '@/types/size';
+import React from 'react';
 
 export type IconProps = {
   name: MdIconName;
@@ -8,20 +9,22 @@ export type IconProps = {
   className?: string;
 };
 
-export const Icon: React.FC<IconProps> = ({
-  name,
-  filled = false,
-  size = 'md',
-  className
-}) => {
-  return (
-    <span
-      slot="icon"
-      className={`material-icons material-icons${
-        filled ? '' : '-outlined'
-      } ${className}`}
-    >
-      {name}
-    </span>
-  );
-};
+export const Icon = React.forwardRef<HTMLSpanElement, IconProps>(
+  (props, ref) => {
+    const { name, filled = false, size = 'md', className } = props;
+
+    return (
+      <span
+        ref={ref}
+        slot="icon"
+        className={`material-icons material-icons${
+          filled ? '' : '-outlined'
+        } ${className}`}
+      >
+        {name}
+      </span>
+    );
+  }
+);
+
+Icon.displayName = 'Icon';
