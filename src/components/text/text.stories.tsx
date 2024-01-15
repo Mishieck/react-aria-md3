f1574676e3e0f7a4e23e194ee9a23ad65b55da65
Mishieck <mishieckmwale@gmail.com>
2024-01-15 12:49:52 +0200

    test(components/text): add arguments to story

--- src/components/text/text.stories.tsx
+++ src/components/text/text.stories.tsx
@@ -14,5 +14,9 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {}
+  args: {
+    scale: 'display',
+    element: 'h1',
+    children: 'Text'
+  }
 };
