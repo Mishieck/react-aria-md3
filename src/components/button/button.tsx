2d5c6a6f3b93cb1912040f192602403447c6818c
Mishieck <mishieckmwale@gmail.com>
2024-01-15 11:07:29 +0200

    feat(components/button): use react-aria and class-variance-authority

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,17 +1,68 @@
 import { ContainerShape } from '@/types/shape';
 import { ContainerVariant } from '@/types/container';
+import { Button as RacButton } from 'react-aria-components';
 import { Maybe } from '../logical/Maybe';
+import { VariantProps, cva } from 'class-variance-authority';
+import { cls } from '@/utils/cls';
+
+type ButtonVariantProps = {
+  container: Record<ContainerVariant, string>;
+  shape: Record<ContainerShape, string>;
+};
+
+const buttonVariants = cva<ButtonVariantProps>(
+  `px-6 py-2.5 flex items-center gap-x-2`,
+  {
+    variants: {
+      container: {
+        filled: 'bg-md-sys-color-primary text-md-sys-color-on-primary',
+        outlined:
+          'border border-md-sys-color-outline text-md-sys-color-primary',
+        elevated:
+          'shadow-lg bg-md-sys-color-surface text-md-sys-color-on-surface',
+        text: 'text-md-sys-color-primary',
+        tonal:
+          'bg-md-sys-color-secondary-container text-md-sys-color-on-secondary-container'
+      },
+      shape: {
+        pill: 'rounded-full',
+        rounded: 'rounded-md',
+        sharp: ''
+      }
+    },
+    defaultVariants: {
+      container: 'filled',
+      shape: 'pill'
+    }
+  }
+);
 
 export type ButtonProps = Partial<{
-  variant: ContainerVariant;
-  shape: ContainerShape;
-  children: React.ReactNode;
-  icon: React.ReactElement;
+  container?: ContainerVariant;
+  shape?: ContainerShape;
+  children?: React.ReactNode;
+  className?: string;
+  hasIcon?: boolean;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
-}>;
+}> &
+  VariantProps<typeof buttonVariants>;
 
 export const Button: React.FC<ButtonProps> = props => {
-  const { variant, shape, children, icon, onClick, ...rest } = props;
+  const {
+    container,
+    shape,
+    children,
+    hasIcon = false,
+    onClick,
+    className,
+    ...rest
+  } = props;
 
-  return <button className="">{children}</button>;
+  return (
+    <RacButton
+      className={cls('', buttonVariants({ container, shape, className }))}
+    >
+      {children}
+    </RacButton>
+  );
 };
