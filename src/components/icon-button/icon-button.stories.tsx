dbbbd597ed881cd4954212381a9af1fec5783abd
Mishieck <mishieckmwale@gmail.com>
2024-01-15 13:00:02 +0200

    test(components/icon-button): add story for icon button

---
+++ src/components/icon-button/icon-button.stories.tsx
@@ -0,0 +1,50 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import StoryProvider from '../../providers/story';
+import { IconButton } from './icon-button';
+import { Icon } from '../icon/icon';
+
+const meta = {
+  title: 'IconButton',
+  component: IconButton,
+  tags: [],
+  argTypes: {},
+  decorators: [StoryProvider]
+} satisfies Meta<typeof IconButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    container: 'default',
+    icon: 'check'
+  }
+};
+
+export const Filled: Story = {
+  args: {
+    container: 'filled',
+    icon: 'check'
+  }
+};
+
+export const Outlined: Story = {
+  args: {
+    container: 'outlined',
+    icon: 'check'
+  }
+};
+
+export const Tonal: Story = {
+  args: {
+    container: 'tonal',
+    icon: 'check'
+  }
+};
+
+export const WithChildIcon: Story = {
+  args: {
+    container: 'default',
+    children: <Icon name="check" />
+  }
+};
