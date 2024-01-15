039963f93b07afc1867f2b4ae71b8b916bd8894d
Mishieck <mishieckmwale@gmail.com>
2024-01-15 11:09:32 +0200

    test(components/button): update story - Change story title to 'Button'. - Replace prop 'variant' with 'container'. - Update 'IconButton'.

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -4,7 +4,7 @@ import { Button } from './button';
 import { Icon } from '../icon/icon';
 
 const meta = {
-  title: 'Example/button',
+  title: 'Button',
   component: Button,
   tags: [],
   argTypes: {},
@@ -16,43 +16,48 @@ type Story = StoryObj<typeof meta>;
 
 export const Filled: Story = {
   args: {
-    variant: 'filled',
+    container: 'filled',
     children: 'Filled'
   }
 };
 
 export const Outlined: Story = {
   args: {
-    variant: 'outlined',
+    container: 'outlined',
     children: 'Outlined'
   }
 };
 
 export const Tonal: Story = {
   args: {
-    variant: 'tonal',
+    container: 'tonal',
     children: 'Tonal'
   }
 };
 
 export const Text: Story = {
   args: {
-    variant: 'text',
+    container: 'text',
     children: 'Text'
   }
 };
 
 export const Elevated: Story = {
   args: {
-    variant: 'elevated',
+    container: 'elevated',
     children: 'Elevated'
   }
 };
 
 export const IconButton: Story = {
   args: {
-    variant: 'filled',
-    children: 'Icon',
-    icon: <Icon name="check" />
+    container: 'filled',
+    hasIcon: true,
+    children: (
+      <>
+        <Icon name="check" />
+        <span>With Icon</span>
+      </>
+    )
   }
 };
