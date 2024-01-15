e76b80aa26021024c7f78212276203a8e3d5260b
Mishieck <mishieckmwale@gmail.com>
2024-01-15 18:10:55 +0200

    feat(components/icon): update props Replace prop 'filled' with 'style'.

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -1,25 +1,53 @@
 import { MdIconName } from '@/types/material-icons';
 import { SizeShort } from '@/types/size';
+import { cls } from '@/utils/cls';
+import { cva } from 'class-variance-authority';
 import React from 'react';
+
+export type IconStyle = 'filled' | 'outlined' | 'sharp';
 
 export type IconProps = {
   name: MdIconName;
-  filled?: boolean;
+  style?: IconStyle;
   size?: SizeShort;
   className?: string;
 };
 
+export type IconVariantProps = {
+  style: Record<IconStyle, string>;
+  size: Record<SizeShort, string>;
+};
+
+const iconVariantProps = cva<IconVariantProps>('material-icons ', {
+  variants: {
+    style: {
+      filled: 'material-icons-filled',
+      outlined: 'material-icons-outlined',
+      sharp: 'material-icons-sharp'
+    },
+    size: {
+      xl: 'text-[40px]',
+      lg: 'text-[32px]',
+      md: 'text-[24px]',
+      sm: 'text-[16px]',
+      xs: 'text-[12px]'
+    }
+  },
+  defaultVariants: {
+    style: 'outlined',
+    size: 'md'
+  }
+});
+
 export const Icon = React.forwardRef<HTMLSpanElement, IconProps>(
   (props, ref) => {
-    const { name, filled = false, size = 'md', className } = props;
+    const { name, style, size, className } = props;
 
     return (
       <span
         ref={ref}
         slot="icon"
-        className={`material-icons material-icons${
-          filled ? '' : '-outlined'
-        } ${className}`}
+        className={cls(iconVariantProps({ style, size, className }))}
       >
         {name}
       </span>
