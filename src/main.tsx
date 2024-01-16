ddfc39a4aa801a4e60dabff2c78497a2ac42c00d
Mishieck <mishieckmwale@gmail.com>
2024-01-16 12:17:07 +0200

    feat(main): use app provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import 'material-icons/iconfont/material-icons.css';
+import { AppProvider } from './providers/app.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <AppProvider>
     <App />
-  </React.StrictMode>
+  </AppProvider>
 );
