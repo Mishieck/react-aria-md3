71d76f43a89cd12f6304e5d4a4f7e21718288070
Mishieck <mishieckmwale@gmail.com>
2024-01-20 10:11:09 +0200

    feat(components/switch): add component

---
+++ src/components/switch/switch.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export type SwitchProps = {};
+
+export const Switch = React.forwardRef<HTMLDivElement, SwitchProps>(
+  (props, ref) => {
+    // TODO: Use md-switch
+    return <></>;
+  }
+);
+
+Switch.displayName = 'Switch';
