139b80a9d62e6426c15a403863cd17534b0ef25a
Mishieck <mishieckmwale@gmail.com>
2024-01-21 13:51:24 +0200

    feat(components/navigation-bar): add navigation props to navigation-bar-link

--- src/components/navigation-bar/navigation-bar.tsx
+++ src/components/navigation-bar/navigation-bar.tsx
@@ -9,10 +9,13 @@ import {
 import { Maybe } from '../logical/Maybe';
 
 export type NavigationBarProps = TabListProps<React.ReactElement>;
+
 export type NavigationBarLinkProps = TabProps & {
   label: string;
   badgeLabel?: string;
   path?: string;
+  preventDefault?: boolean;
+  onNavigate?: (path?: string) => void;
 };
 
 export const NavigationBar = React.forwardRef<
@@ -40,32 +43,42 @@ NavigationBar.displayName = 'NavigationBar';
 export const NavigationBarLink = React.forwardRef<
   HTMLDivElement,
   NavigationBarLinkProps
->(({ label, badgeLabel, children, path, ...props }, ref) => {
-  return (
-    <Tab ref={ref} className={cls(`flex justify-between`)} {...props}>
-      {values => (
-        <a
-          href={path ?? '#'}
-          className="group flex w-fit md:w-32 flex-col items-center justify-center px-0 py-4 gap-1"
-        >
-          <div
-            className={cls(
-              `
+>(
+  (
+    { label, badgeLabel, children, path, preventDefault, onNavigate, ...props },
+    ref
+  ) => {
+    const handleNavigate: React.MouseEventHandler = event => {
+      preventDefault && event.preventDefault();
+      onNavigate?.(path);
+    };
+
+    return (
+      <Tab ref={ref} className={cls(`flex justify-between`)} {...props}>
+        {values => (
+          <a
+            href={path ?? '#'}
+            onClick={handleNavigate}
+            className="group flex w-fit md:w-32 flex-col items-center justify-center px-0 py-4 gap-1"
+          >
+            <div
+              className={cls(
+                `
                 relative 
                 rounded-full 
                 w-16 h-8 
                 flex items-center justify-center
               `,
-              {
-                'bg-md-sys-color-secondary-container text-md-sys-color-on-secondary-container':
-                  values.isSelected || values.isHovered
-              }
-            )}
-          >
-            {typeof children === 'function' ? children(values) : children}
-            <Maybe display={!!badgeLabel}>
-              <div
-                className="
+                {
+                  'bg-md-sys-color-secondary-container text-md-sys-color-on-secondary-container':
+                    values.isSelected || values.isHovered
+                }
+              )}
+            >
+              {typeof children === 'function' ? children(values) : children}
+              <Maybe display={!!badgeLabel}>
+                <div
+                  className="
                   absolute top-0 left-8 
                   rounded-full 
                   min-w-[22px] h-4
@@ -75,18 +88,19 @@ export const NavigationBarLink = React.forwardRef<
                   text-[11px] leading-none tracking-[.045em] font-medium 
                   text-md-sys-color-on-error
                 "
-              >
-                {badgeLabel}
-              </div>
-            </Maybe>
-          </div>
-          <span className="text-xs text-neutral-900 dark:text-neutral-100 leading-tight tracking-[.0416em]">
-            {label}
-          </span>
-        </a>
-      )}
-    </Tab>
-  );
-});
+                >
+                  {badgeLabel}
+                </div>
+              </Maybe>
+            </div>
+            <span className="text-xs text-neutral-900 dark:text-neutral-100 leading-tight tracking-[.0416em]">
+              {label}
+            </span>
+          </a>
+        )}
+      </Tab>
+    );
+  }
+);
 
 NavigationBarLink.displayName = 'NavigationBarLink';
